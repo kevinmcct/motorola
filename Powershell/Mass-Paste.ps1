20bd2a42f8c3fc42654cdd60de34fae3d325e55c
Kevin McCarty <109972949+kevinmcct@users.noreply.github.com>
2023-11-19 22:36:48 -0600

    Update Mass-Paste.ps1

--- Powershell/Mass-Paste.ps1
+++ Powershell/Mass-Paste.ps1
@@ -1,16 +1,17 @@
 $file = "C:\Temp\MFF-Contacts.txt"
+$Prefix = "MF"
 Add-Content -Path $file -Value '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <config>
   <category name="PCRContacts">'
 For($i=0; $i -lt 201; $i++){
   $ContactNumber = $i.ToString("000")
-  Add-Content -Path $file -Value "    <set name=&PCRContacts& alias=&MF$($ContactNumber)&>
-      <field name=&ContactName&>MF$($ContactNumber)</field>
+  Add-Content -Path $file -Value "    <set name=&PCRContacts& alias=&$($Prefix)$($ContactNumber)&>
+      <field name=&ContactName&>$($Prefix)$($ContactNumber)</field>
       <collection name=&MDCCalls& />
       <collection name=&QuikCallIICalls& />
       <collection name=&DigitalCalls&>
         <set name=&DigitalCalls& index=&0& key=&Private Call&>
-          <field name=&DU_CALLALIAS&>MF$($ContactNumber)</field>
+          <field name=&DU_CALLALIAS&>$($Prefix)$($ContactNumber)</field>
           <field name=&DU_CALLLSTID&>1$($ContactNumber)</field>
           <field name=&DU_ROUTETYPE&>Regular</field>
           <field name=&DU_CALLPRCDTNEN&>Yes</field>
